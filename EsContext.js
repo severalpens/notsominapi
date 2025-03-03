71c94458a1136049602c883126bc6e6c6af3f8bf
Paul Collins <paul.collins@severalpens.com>
2025-03-03 14:16:26 +1100

    Refactor EsContext to use environment variables for Elasticsearch connection and improve connection handling

--- EsContext.js
+++ EsContext.js
@@ -4,18 +4,12 @@ const { Client } = require('@elastic/elasticsearch');
 const chalk = require('chalk');
 
 
-//Connection Settings Lookup IDs-----------------------------------
-const cnType = 'api'; // 'api' or 'user'
-const clusterId = 1; // (1 =  https://d8ab8a92...)
-const userId = 1; // (1 =  ibm_cloud_ad...)
-const apiKeyId = 1; // (1 =  dummy_key1)
-const indexId = 1; // (1 =  dummy_index)
-//-----------------------------------------------
-
 class EsContext {
-    dbContext = null;
-    apiKeyProps = { url: '', key: '' };
-    userProps = { url: '', username: '', password: '' };
+    userProps = {
+         url: process.env.ELASTICSEARCH_URL, 
+         username: process.env.ELASTICSEARCH_USERNAME, 
+         password: process.env.ELASTICSEARCH_PASSWORD
+        };
     client = null;
     
     getAllData = async (indexName) => {
@@ -32,23 +26,8 @@ class EsContext {
     }
 
     setConnectionDetails = async () => {
-        if (cnType === 'api') {
-            this.apiKeyProps = await this.dbContext.getApiKey(clusterId, apiKeyId);
+
             this.client = new Client({
-                node: this.apiKeyProps.url,
-                auth: {
-                    apiKey: this.apiKeyProps.key
-                },
-                tls: {
-                    rejectUnauthorized: false
-                }
-            });
-        }
-
-        if (cnType === 'user') {
-            this.userProps = await this.dbContext.getUser(clusterId, userId);
-
-            client = new Client({
                 node: this.userProps.url,
                 auth: {
                     username: this.userProps.username,
@@ -58,21 +37,18 @@ class EsContext {
                     rejectUnauthorized: false
                 }
             });
-        }
     }
 
     verifyClientConnection = async () => {
         let isConnected = false;
         try {
             const resp = await this.client.info();
-            cnType === 'user' ? console.log(chalk.green(`Connected to ElasticSearch user: ${this.userProps.username}`)) :
-                console.log(chalk.green(`Connected to ElasticSearch using api: ${this.apiKeyProps.key.substring(0, 6)}..`));
+             console.log(chalk.green(`Connected to ElasticSearch user: ${this.userProps.username}`));
             isConnected = true;
             return isConnected;
         }
         catch (e) {
-            cnType === 'user' ? console.log(chalk.red(`Failed to connect to ElasticSearch using user credentials: ${JSON.stringify(this.userProps)}`)) :
-                console.log(chalk.red(`Failed to connect to ElasticSearch using api credentials: ${JSON.stringify(this.apiKeyProps)}`));
+            console.log(chalk.red(`Failed to connect to ElasticSearch using user credentials: ${JSON.stringify(this.userProps)}`)) ;
             console.log(e);
             isConnected = false;
             return isConnected;
