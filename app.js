34463245cb931f7a3d46180d1ad0b0006c50822c
Paul Collins <paul.collins@severalpens.com>
2025-03-10 13:30:57 +1100

    update

--- app.js
+++ app.js
@@ -121,10 +121,8 @@ const esContext = new EsContext();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
